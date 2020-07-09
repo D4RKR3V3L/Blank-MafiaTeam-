9f7e02040dd5955277c5196cc8ce8f9bc5c8db7f
Md Rakib <67947669+darkrevel@users.noreply.github.com>
2020-07-09 16:56:45 +0600

    Update blank-mafia.py

--- blank-mafia.py
+++ blank-mafia.py
@@ -462,8 +462,8 @@ def pilih_super():
     ╭╱╱▕╋╋╋╋▏╲╲╮
     ╰╯╯┈╲▂▂╱┈╰╰╯
     👉👉👉👉We make a system and fuck any system👈👈👈👈
-    👉👉👉👉Blank Mafia Father of abal👈👈👈👈"""
-   print"\033[1;97m 👹👹👹👹 Dark Revel Rakib_Vau👹👹👹👹"
+    👉👉👉👉Blank Mafia Father of abal👈👈👈👈
+    👹👹👹👹 Dark Revel Rakib_Vau👹👹👹👹"""
 
 
 raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
