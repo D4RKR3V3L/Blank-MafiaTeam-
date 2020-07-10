6d6a1f4e44eec6120657736f6b3f0c7e599302f0
Md Rakib <67947669+darkrevel@users.noreply.github.com>
2020-07-10 15:49:22 +0600

    Update blank-mafia.py

--- blank-mafia.py
+++ blank-mafia.py
@@ -467,12 +467,11 @@ def pilih_super():
 raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
 
 print"\033[	1;96mDark Revel Rakib_vau member on Blank Mafia Team"
-
-name = input ("enter your name: ")
-age = input ("enter you age: ")
-distric = input (" enter you distric name: ")
-
-print("Hello" + name + "!"  "You are" + age + "Your distric is" + info )
+				name = input ("Enter your name: ")
+				age = input (" Enter your age: ")
+				distric = input (" Enter your distric name: ")
+     
+print("Hello " + name + "!"  "You are " + age + "Your distric is " + distric)
 raw_input("\n\000[1;96m[\033[1;97mBack\033[1;96m]")
 
 menu()
