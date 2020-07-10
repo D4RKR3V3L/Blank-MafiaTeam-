a421c9bd25ab3dc6ef132d8957a209141d475263
Md Rakib <67947669+darkrevel@users.noreply.github.com>
2020-07-10 13:03:39 +0600

    Update blank-mafia.py

--- blank-mafia.py
+++ blank-mafia.py
@@ -468,9 +468,9 @@ raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
 
 print"\033[1;96mDark Revel Rakib_vau member on Blank Mafia Team"
 
-name = input ("Enter your name: ")
-age = input ("Enter is your age: ")
-info = input  ("Enter your distric name: ")
+name = input ("\033[1;96mEnter your name: ")
+age = input ("\033[1;96mEnter is your age: ")
+info = input  ("\033[1;96mEnter your distric name: ")
 
 print("Hello" + name + "!"  "You are" + age + "Your distric is" + info )
 raw_input("\n\000[1;96m[\033[1;97mBack\033[1;96m]")
