86c0fbf71991307f5c752647700afd7f658c5aac
Md Rakib <67947669+darkrevel@users.noreply.github.com>
2020-07-10 16:38:45 +0600

    Update blank-mafia.py

--- blank-mafia.py
+++ blank-mafia.py
@@ -470,7 +470,7 @@ print"\033[1;96mDark Revel Rakib_vau member on Blank Mafia Team"
 
 janu = input("Enter your name: ")
 
-print"\033[1;96mHello" + janu + "!"
+print("\033[1;96mHello" + janu + "!")
 
 raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
 
