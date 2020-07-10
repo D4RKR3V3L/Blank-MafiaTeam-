8cc2311c8c121c10afb927565b4aea287dc2eeda
Md Rakib <67947669+darkrevel@users.noreply.github.com>
2020-07-10 12:41:24 +0600

    Update blank-mafia.py

--- blank-mafia.py
+++ blank-mafia.py
@@ -455,7 +455,7 @@ def pilih_super():
 	print("\033[1;96m[+] \033[1;92mTHANKS FOR USING MY COMMANDS ! WE WILL BE RIGHT BACK \033[1;91m: \033[1;97mout/checkpoint.txt")
 	print """
 	
-	╭╮╮╱▔▔▔▔╲╭╭╮
+    ╭╮╮╱▔▔▔▔╲╭╭╮
     ╰╲╲▏▂╲╱▂▕╱╱╯
     ┈┈╲▏▇▏▕▇▕╱┈┈
     ┈┈╱╲▔▕▍▔╱╲┈┈
